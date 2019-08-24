84511ba49891e10abf38e52f92fac7c1df297090
Alex Berk <aleks.berkov@gmail.com>
2019-08-24 15:25:34 +0300

    Added option to specify text for link button

--- bot_notifier.py
+++ bot_notifier.py
@@ -7,19 +7,23 @@ class BotNotifier():
 		self.token = token
 		self.admin_chat_id = admin_chat_id
 
-	def send_message(self, message, url=None, chat_id=None):
+	def send_message(self, message, chat_id=None, link=None, disable_preview=False):
 		if not chat_id: chat_id = self.admin_chat_id
-		params = {'chat_id': chat_id, 'text': message, 'parse_mode':'html', 'disable_web_page_preview': True}
-		if url:
-			params['reply_markup'] = json.dumps({'inline_keyboard': [[{'text': 'Просмотреть', 'url': url}]]})
+		params = {'chat_id': chat_id, 'text': message, 'parse_mode':'html', 'disable_web_page_preview': disable_preview}
+		if link:
+			try:
+				anchor, url = link
+			except ValueError:
+				anchor, url = ('Просмотреть', link)
+			params['reply_markup'] = json.dumps({'inline_keyboard': [[{'text': anchor, 'url': url}]]})
 		r = requests.post(f'https://api.telegram.org/bot{self.token}/sendMessage', params=params)
 		success = json.loads(r.text)['ok']
 		if not success:
-			print('Message not been sent!, Got response:', r.text, message, url, sep='\n')
+			print('Message not been sent!, Got response:', r.text, message, link, sep='\n')
 		return success
 
 	def send_job(self, job):
 		tags = ', '.join(job['tags'])
 		price = job['price'] + ' <i>за час</i>' if job['price_format'] == 'per_hour' else job['price']
 		text = f"<b>{job['title']}</b>\n{price}\n<code>{tags}</code>"
-		self.send_message(text, job['url'])
+		self.send_message(text, link=job['url'])
