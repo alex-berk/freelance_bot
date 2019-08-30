162076c76f4387aee75aabf2aa7d7972d6e1fe10
Alex Berk <aleks.berkov@gmail.com>
2019-08-30 13:40:26 +0300

    Moved bot listeners from __init__ to main module, added encoding definition

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import os, logging
 from multiprocessing import Process
 import time, datetime
@@ -31,6 +33,8 @@ def set_file_logger():
 	logger.addHandler(file_handler)
 set_file_logger()
 
+bot = tgbot.BotNotifier(os.environ['BOT_TOKEN'], os.environ['CHAT_ID'])
+
 def string_cleaner(dirty_srtring):
 	word_list, curr_word = [], ''
 	for s in dirty_srtring:
@@ -48,7 +52,11 @@ def tasks_sender(task_list):
 		relevant_users = db_handler.get_relevant_users_ids(search_body)
 		if relevant_users: logger.debug(f"Found task {task['id'], task['tags']} for the users {relevant_users}")
 		for user_id in relevant_users:
-			bot.send_job(task, user_id)
+			logger.debug(f"Sending task {task['id']}")
+			tags = ', '.join(task['tags'])
+			price = task['price'] + ' <i>за час</i>' if task['price_format'] == 'per_hour' else task['price']
+			text = f"<b>{task['title']}</b>\n{price}\n<code>{tags}</code>"
+			bot.send_message(text, link=task['url'], chat_id=user_id, disable_preview=True)
 
 def parse_tasks(retry=False):
 	url = 'https://freelansim.ru/tasks?per_page=25&page=1'
@@ -90,10 +98,76 @@ def main():
 		time.sleep(60 * 5)
 
 def subprocess():
+	def setup_keys(chat_id):
+		bot.setup_step[chat_id] = 'setup_keys'
+		setup_text = 'Сейчас можно будет задать ключевые слова для поиска.\nКаждый раз, когда бот будет находить их в задаче, вам придет оповещение.\nКлючи разделяются запятой.\nДопускается использование только букв, цифр и пробелов\nПоиск осуществляется по тегам и отдельным словам из заголовков, так что лучше задавать однословные ключи.\n\n<code>Пример:</code>\n<code>node js, java script, js, фронтенд</code>'
+		current_keys = db_handler.get_user_skeys(chat_id)
+		if current_keys: bot.send_message(f'Ваши текущие ключевые слова для поиска:\n<b>{", ".join(current_keys)}</b>', chat_id)
+		bot.send_message(setup_text, chat_id, force_reply=True, keyboard=['Отмена'])
+
+
+	@bot.message_handler(commands=['status', 'start', 'keywords', 'cancel', 'stop'])
+	def handle_commands(message):
+		logger.debug(f'Got message from @{message.from_user.username}, id{message.from_user.id} in chat {message.chat.id}, {message.chat.title if message.chat.title else message.chat.type}, with text "{message.text}"')
+		if bot.verify_command(message.text, 'status'):
+			status_text = 'Up and running!'
+			if bot.setup_step.get(message.chat.id): status_text += '\nCurrent setup step: ' + bot.setup_step[message.chat.id]
+			bot.send_message(status_text, message.chat.id)
+		
+		elif bot.verify_command(message.text, 'start'):
+			bot.setup_step[message.chat.id] = None
+			if db_handler.get_user_skeys(message.chat.id):
+				bot.send_message('У вас уже настроены ключевые слова для поиска.\nЗаново их задать можно коммандой /keywords', message.chat.id)
+			else:
+				setup_keys(message.chat.id)
+		
+		elif bot.verify_command(message.text, 'keywords'):
+			bot.setup_step[message.chat.id] = None
+			setup_keys(message.chat.id)
+
+		elif bot.verify_command(message.text, 'stop'):
+			bot.setup_step[message.chat.id] = 'stop_tacking'
+			bot.send_message('Вы точно хотите остановить отслеживание?\n(Нажмите "Да" чтобы подтвердить, "Нет" чтобы продолжить получать уведомления)', message.chat.id, force_reply=True, keyboard=['Да', 'Нет'])
+
+		elif bot.verify_command(message.text, 'cancel'):
+			bot.setup_step[message.chat.id] = None
+			bot.send_message('Действие отменено', message.chat.id)
+		
+	@bot.message_handler(content_types=['text'])
+	def handle_text(message):
+		if message.text.lower() in ['нет', 'отмена']:
+			if bot.setup_step[message.chat.id]:
+				bot.setup_step[message.chat.id] = None
+				bot.send_message('Действие отменено', message.chat.id)
+			else:
+				bot.send_message('Нечего отменить', message.chat.id)
+		
+		elif bot.setup_step.get(message.chat.id) == 'setup_keys':
+			s_keys = parse_string(message.text)
+			try:
+				db_handler.add_user(message.chat.id, s_keys)
+			except db_handler.sqlite3.IntegrityError:
+				db_handler.update_user_keys(message.chat.id, s_keys)
+			confirm_text = 'Все готово. Ваши ключевые слова для поиска:\n<b>' + ", ".join(s_keys) + '</b>\n\nНачинаю отслеживать задачи'
+			bot.send_message(confirm_text, message.chat.id)
+			bot.send_sticker('CAADAgADBwIAArD72weq7luNKMN99BYE', message.chat.id)
+			bot.setup_step[message.chat.id] = None
+		
+		elif bot.setup_step.get(message.chat.id) == 'stop_tacking':
+			if message.text.lower() == 'да':
+				db_handler.delete_user(message.chat.id)
+				bot.send_message("Отслеживание остановлено. Снова начать отслеживать задачи можно если набрать комманду /start", message.chat.id)
+				bot.setup_step[message.chat.id] = None
+		else:
+			logger.debug(f'Got random message from @{message.from_user.username}, id{message.from_user.id} in chat {message.chat.id}, {message.chat.title if message.chat.title else message.chat.type}, with text "{message.text}"')
+
+	@bot.callback_query_handler(func=lambda call:True)
+	def test_callback(call):
+		logger.info(f'Got callback_query {call}')
+	
+
 	bot.polling()
 
-
-bot = tgbot.BotNotifier(os.environ['BOT_TOKEN'], os.environ['CHAT_ID'])
 
 if __name__ == '__main__':
 	os.system('cls' if os.name=='nt' else 'clear')
--- tgbot.py
+++ tgbot.py
@@ -29,60 +29,6 @@ class BotNotifier(TeleBot):
 		self.username = self.get_me().username
 		self.setup_step = {}
 
-		@self.message_handler(commands=['status', 'start', 'keywords', 'cancel', 'stop'])
-		def handle_commands(message):
-			logger.debug(f'Got message from @{message.from_user.username}, id{message.from_user.id} in chat {message.chat.id}, {message.chat.title if message.chat.title else message.chat.type}, with text "{message.text}"')
-			if self.verify_command(message.text, 'status'):
-				status_text = 'Up and running!'
-				if self.setup_step.get(message.chat.id): status_text += '\nCurrent setup step: ' + self.setup_step[message.chat.id]
-				self.send_message(status_text, message.chat.id)
-			elif self.verify_command(message.text, 'start'):
-				self.setup_step[message.chat.id] = None
-				if db_handler.get_user_skeys(message.chat.id):
-					self.send_message('У вас уже настроены ключевые слова для поиска.\nЗаново их задать можно коммандой /keywords', message.chat.id)
-				else:
-					self.setup_keys(message.chat.id)
-			elif self.verify_command(message.text, 'keywords'):
-				self.setup_step[message.chat.id] = None
-				self.setup_keys(message.chat.id)
-			elif self.verify_command(message.text, 'cancel'):
-				self.setup_step[message.chat.id] = None
-				self.send_message('Действие отменено', message.chat.id)
-			elif self.verify_command(message.text, 'stop'):
-				self.setup_step[message.chat.id] = 'stop_tacking'
-				self.send_message('Вы точно хотите остановить отслеживание?\n(Нажмите "Да" чтобы подтвердить, "Нет" чтобы продолжить получать уведомления)', message.chat.id, force_reply=True, keyboard=['Да', 'Нет'])
-
-		@self.message_handler(content_types=['text'])
-		def handle_text(message):
-			if message.text.lower() in ['нет', 'отмена']:
-				if self.setup_step[message.chat.id]:
-					self.setup_step[message.chat.id] = None
-					self.send_message('Операция отменена', message.chat.id)
-				else:
-					self.send_message('Нечего отменить', message.chat.id)
-			elif self.setup_step.get(message.chat.id) == 'setup_keys':
-				s_keys = parse_string(message.text)
-				try:
-					db_handler.add_user(message.chat.id, s_keys)
-				except db_handler.sqlite3.IntegrityError:
-					db_handler.update_user_keys(message.chat.id, s_keys)
-				confirm_text = 'Все готово. Ваши ключевые слова для поиска:\n<b>' + ", ".join(s_keys) + '</b>\n\nНачинаю отслеживать задачи'
-				self.send_message(confirm_text, message.chat.id)
-				self.send_sticker('CAADAgADBwIAArD72weq7luNKMN99BYE', message.chat.id)
-				self.setup_step[message.chat.id] = None
-			elif self.setup_step.get(message.chat.id) == 'stop_tacking':
-				if message.text.lower() == 'да':
-					db_handler.delete_user(message.chat.id)
-					self.send_message("Отслеживание остановлено. Снова начать отслеживать задачи можно если набрать комманду /start", message.chat.id)
-					self.setup_step[message.chat.id] = None
-			else:
-				logger.debug(f'Got random message from @{message.from_user.username}, id{message.from_user.id} in chat {message.chat.id}, {message.chat.title if message.chat.title else message.chat.type}, with text "{message.text}"')
-
-		@self.callback_query_handler(func=lambda call:True)
-		def test_callback(call):
-			logger.info(f'Got callback_query {call}')
-
-
 	def verify_command(self, text, command):
 		return text == '/' + command or text == ''.join(['/', command, '@', self.username])
 
@@ -129,17 +75,3 @@ class BotNotifier(TeleBot):
 			r = requests.post(f'https://api.telegram.org/bot{self.token}/sendSticker', params=params)
 		except Exception as e:
 			logger.error(e)
-
-	def send_job(self, job, chat_id=None):
-		logger.debug(f"Sending job {job['id']}")
-		tags = ', '.join(job['tags'])
-		price = job['price'] + ' <i>за час</i>' if job['price_format'] == 'per_hour' else job['price']
-		text = f"<b>{job['title']}</b>\n{price}\n<code>{tags}</code>"
-		self.send_message(text, link=job['url'], chat_id=chat_id, disable_preview=True)
-
-	def setup_keys(self, chat_id):
-		self.setup_step[chat_id] = 'setup_keys'
-		setup_text = 'Сейчас можно будет задать ключевые слова для поиска.\nКаждый раз, когда бот будет находить их в задаче, вам придет оповещение.\nКлючи разделяются запятой.\nДопускается использование только букв, цифр и пробелов\nПоиск осуществляется по тегам и отдельным словам из заголовков, так что лучше задавать однословные ключи.\n\n<code>Пример:</code>\n<code>node js, java script, js, фронтенд</code>'
-		current_keys = db_handler.get_user_skeys(chat_id)
-		if current_keys: self.send_message(f'Ваши текущие ключевые слова для поиска:\n<b>{", ".join(current_keys)}</b>', chat_id)
-		self.send_message(setup_text, chat_id, force_reply=True, keyboard=['Отмена'])
