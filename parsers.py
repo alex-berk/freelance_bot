a7874435627eded7189eddb34a8b505f87c21741
Alex Berk <aleks.berkov@gmail.com>
2019-09-13 17:22:32 +0300

    Made parser and json_parser into class and subclass

--- parsers.py
+++ parsers.py
@@ -1,64 +1,80 @@
 import requests
 from lxml import html
 import json
+import concurrent.futures
 
-def parse_freelansim(url, containers, headers=False, **extractors):
-	query_params = {'url': url}
-	if headers: query_params['headers'] = headers
-	try:
-		r = requests.get(**query_params)
-	except (TimeoutError, ConnectionError) as e:
-		return {'error': e}
+class Parser:
+	instances = []
 
-	containers = json.loads(r.text)[containers]
-	parsed_objcts = []
-	for objct in containers:
-		extracted_fields = {}
-		for field_name, extractor in extractors.items():
-			extractor, *subextractor = extractor.split('//')
-			extractor = extractor.split('/')
+	def __init__(self, url, containers, headers=False, **extractors):
+		self.url = url
+		self.containers = containers
+		self.headers = headers
+		self.extractors = extractors
 
-			curr_field = objct
-			try:
-				while extractor:
-					curr_field = curr_field[extractor.pop(0)]
-				if subextractor:
-					extracted_fields[field_name] = [item[subextractor[0]].replace('\u202f', ' ').replace('\u200b', '').strip() for item in curr_field]
+		self.__class__.instances.append(self)
+
+	def parse(self):
+		query_params = {'url': self.url}
+		if self.headers: query_params['headers'] = self.headers
+		try:
+			r = requests.get(**query_params)
+		except (TimeoutError, ConnectionError) as e:
+			return {'error': e}
+
+		tree = html.fromstring(r.text)
+		containers = tree.xpath(self.containers)
+
+		parsed_objcts = []
+		for objct in containers:
+			extracted_fields = {}
+			for field_name, extractor in self.extractors.items():
+				if extractor:
+					try:
+						extracted_fields[field_name] = objct.xpath('.' + extractor)[0]
+						extracted_fields[field_name] = extracted_fields[field_name].replace('\u202f', ' ').replace('\u200b', '').strip()
+					except IndexError:
+						extracted_fields[field_name] = None
 				else:
-					extracted_fields[field_name] = curr_field.replace('\u202f', ' ').replace('\u200b', '').strip()
-			except KeyError:
-				extracted_fields[field_name] = None
-		parsed_objcts.append(extracted_fields)
-
-	return parsed_objcts
-
-
-def parse_freelancehunt(url, containers, headers=False, **extractors):
-	query_params = {'url': url}
-	if headers: query_params['headers'] = headers
-	try:
-		r = requests.get(**query_params)
-	except (TimeoutError, ConnectionError) as e:
-		return {'error': e}
-
-	tree = html.fromstring(r.text)
-	containers = tree.xpath(containers)
-
-	parsed_objcts = []
-	for objct in containers:
-		extracted_fields = {}
-		for field_name, extractor in extractors.items():
-			if extractor:
-				try:
-					extracted_fields[field_name] = objct.xpath('.' + extractor)[0]
-					extracted_fields[field_name] = extracted_fields[field_name].replace('\u202f', ' ').replace('\u200b', '').strip()
-				except IndexError:
 					extracted_fields[field_name] = None
-			else:
-				extracted_fields[field_name] = None
-		parsed_objcts.append(extracted_fields)
+			parsed_objcts.append(extracted_fields)
 
-	return parsed_objcts
+		return parsed_objcts
+
+
+class JsonParser(Parser):
+	def __init__(self, url, containers, headers=False, **extractors):
+		super().__init__(url, containers, headers, **extractors)
+
+	def parse(self):
+		query_params = {'url': self.url}
+		if self.headers: query_params['headers'] = self.headers
+		try:
+			r = requests.get(**query_params)
+		except (TimeoutError, ConnectionError) as e:
+			return {'error': e}
+
+		containers = json.loads(r.text)[self.containers]
+		parsed_objcts = []
+		for objct in containers:
+			extracted_fields = {}
+			for field_name, extractor in self.extractors.items():
+				extractor, *subextractor = extractor.split('//')
+				extractor = extractor.split('/')
+
+				curr_field = objct
+				try:
+					while extractor:
+						curr_field = curr_field[extractor.pop(0)]
+					if subextractor:
+						extracted_fields[field_name] = [item[subextractor[0]].replace('\u202f', ' ').replace('\u200b', '').strip() for item in curr_field]
+					else:
+						extracted_fields[field_name] = curr_field.replace('\u202f', ' ').replace('\u200b', '').strip()
+				except KeyError:
+					extracted_fields[field_name] = None
+			parsed_objcts.append(extracted_fields)
+
+		return parsed_objcts
 
 
 flnsm_params = {
@@ -87,8 +103,11 @@ frlnchnt_params = {
 	'tags': ''
 }
 
-for index, task in enumerate(parse_freelansim(**flnsm_params)):
+freelansim = JsonParser(**flnsm_params)
+freelancehunt = Parser(**frlnchnt_params)
+
+for index, task in enumerate(freelansim.parse()):
 	print(index, task)
 
-for index, task in enumerate(parse_freelancehunt(**frlnchnt_params)):
+for index, task in enumerate(freelancehunt.parse()):
 	print(index, task)
