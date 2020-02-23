2ce1ed25d683abaec545f17e0eb4452a61e77250
Alex Berk <aleks.berkov@gmail.com>
2020-02-23 18:25:51 +0200

    str.startswith on log_parser

--- log_parser.py
+++ log_parser.py
@@ -24,7 +24,7 @@ def search_last_telegram_response(log):
 def get_all_hosts(log):
 	hosts = set()
 	for line in log:
-		if line.event[:23] == 'Sending request to http':
+		if line.event.startswith('Sending request to http'):
 			hosts.add(line.event[19:])
 	return hosts
 
@@ -39,7 +39,7 @@ def get_last_parsing(log):
 
 def get_new_tasks_q(log):
 	new_tasks = []
-	new_task_lines = [line.event[10:] for line in log if line.event[:9] == 'New tasks' and line.event != 'New tasks []']
+	new_task_lines = [line.event[10:] for line in log if line.event.startswith('New tasks') and line.event != 'New tasks []']
 	for line in new_task_lines:
 		new_tasks.extend(json.loads(line.replace('\'', '"')))
 	hosts = Counter()
@@ -49,7 +49,7 @@ def get_new_tasks_q(log):
 
 def get_sent_tasks_q(log):
 	sent_tasks = []
-	sent_task_lines = [line.event.split('\'')[1] for line in log if line.event[:10] == 'Found task']
+	sent_task_lines = [line.event.split('\'')[1] for line in log if line.event.startswith('Found task')]
 	tasks = Counter()
 	tasks.update([line.split('/')[2] for line in sent_task_lines])
 	return tasks
@@ -57,7 +57,7 @@ def get_sent_tasks_q(log):
 def get_sent_messages(log):
 	users = Counter()
 	sent_tasks = []
-	sent_task_lines = [line.event.split('for the users ')[1] for line in log if line.event[:10] == 'Found task']
+	sent_task_lines = [line.event.split('for the users ')[1] for line in log if line.event.startswith('Found task')]
 	users_ids = [id[1:-1].split(', ') for id in sent_task_lines]
 	users_ids_flatten = [item for sublist in users_ids for item in sublist]
 	users.update(users_ids_flatten)
