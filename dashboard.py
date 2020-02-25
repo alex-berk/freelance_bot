1006b28fabe2ffb9daf199e2f2800608358992ad
Alex Berk <aleks.berkov@gmail.com>
2020-02-26 00:21:06 +0200

    Added second axis to the chart

--- dashboard.py
+++ dashboard.py
@@ -18,14 +18,18 @@ def home():
 	lp = log_parser.get_last_parsing()
 	nt = log_parser.get_new_tasks_q()
 	st = log_parser.get_sent_tasks_q()
+	stdy = log_parser.get_sent_tasks_q_wdays()
+	ntdy = log_parser.get_new_tasks_q_wdays()
 
-	legend_days = [i[0] for i in sorted(log_parser.get_new_tasks_q_wdays().items())]
-	wnt = [i[1] for i in sorted(log_parser.get_new_tasks_q_wdays().items())]
-	wst = [i[1] for i in sorted(log_parser.get_sent_tasks_q_wdays().items())]
+	proc_sent_graph = list( map(lambda x, y: round((x[1] / y[1]) * 100, 2), sorted(stdy.items()), sorted(ntdy.items())) )
+
+	legend_days = [i[0] for i in sorted(ntdy.items())]
+	wnt = [i[1] for i in sorted(ntdy.items())]
+	wst = [i[1] for i in sorted(stdy.items())]
 
 	proc_sent = {hostname: st.get(hostname, 0) / nt[hostname] * 100 if nt[hostname] != 0 else 0 for hostname in nt}
 
-	return render_template('home.html', title='Parsing Stats', lt=lt, lp=lp, nt=nt, st=st, proc_sent=proc_sent, wnt=wnt, wst=wst, legend_days=legend_days)
+	return render_template('home.html', title='Parsing Stats', lt=lt, lp=lp, nt=nt, st=st, proc_sent=proc_sent, wnt=wnt, wst=wst, legend_days=legend_days, proc_sent_graph=proc_sent_graph)
 
 @app.route('/users')
 def users():
