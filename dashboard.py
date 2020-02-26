3de7eff777415e31f17b39acc9b27305c120a25b
Alex Berk <aleks.berkov@gmail.com>
2020-02-26 02:13:55 +0200

    Added filterting on chart

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -12,14 +12,24 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_TOKEN')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+	if request.method == 'POST':
+		site_filter = request.json.get('site_filter')
+		ntdy = log_parser.get_new_tasks_q_wdays(site=site_filter)
+		stdy = log_parser.get_sent_tasks_q_wdays(site=site_filter)
+		
+		proc_sent_graph = list( map(lambda x, y: round((x[1] / y[1]) * 100, 2), sorted(stdy.items()), sorted(ntdy.items())) )
+		ntdy = [i[1] for i in sorted(ntdy.items())]
+		stdy = [i[1] for i in sorted(stdy.items())]
+		return jsonify({'ntdy': ntdy, 'stdy': stdy, 'proc_sent_graph': proc_sent_graph})
+
 	lt = log_parser.get_last_telegram_response()
 	lp = log_parser.get_last_parsing()
 	nt = log_parser.get_new_tasks_q()
 	st = log_parser.get_sent_tasks_q()
-	stdy = log_parser.get_sent_tasks_q_wdays()
 	ntdy = log_parser.get_new_tasks_q_wdays()
+	stdy = log_parser.get_sent_tasks_q_wdays()
 
 	proc_sent_graph = list( map(lambda x, y: round((x[1] / y[1]) * 100, 2), sorted(stdy.items()), sorted(ntdy.items())) )
 
