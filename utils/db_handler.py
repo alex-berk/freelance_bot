3c0d95d3004e660303cccde198ed56d30f9ac968
Alex Berk <aleks.berkov@gmail.com>
2020-03-06 16:05:34 +0200

    Added quey_db(), fixed keywords list in get_users()

--- utils/db_handler.py
+++ utils/db_handler.py
@@ -7,126 +7,96 @@ logger = logging.getLogger('__main__')
 User = namedtuple('User', ['chat_id', 'keywords', 'lang', 'name', 'active'])
 
 db_loc = 'data.db'
-conn = sqlite3.connect(db_loc)
-cursor = conn.cursor()
 
-try:
-	cursor.execute('''CREATE TABLE users(
-						id integer primary key,
-						search_keys text,
-						lang text NOT NULL DEFAULT 'rus',
-						nickname text,
-						active integer NOT NULL DEFAULT 1,
-					)''')
-	conn.commit()
-except sqlite3.OperationalError:
-	pass
+def create_db():
+	conn = sqlite3.connect(db_loc)
+	cursor = conn.cursor()
+	try:
+		cursor.execute('''CREATE TABLE users(
+							id integer primary key,
+							search_keys text,
+							lang text NOT NULL DEFAULT 'rus',
+							nickname text,
+							active integer NOT NULL DEFAULT 1,
+						)''')
+		conn.commit()
+	except sqlite3.OperationalError:
+		pass
 
-try:
-	cursor.execute('''CREATE TABLE tasks(
-						link text primary key,
-						title text,
-						tags text,
-						price text,
-						price_format text
-					)''')
-	conn.commit()
-except sqlite3.OperationalError:
-	pass
+	try:
+		cursor.execute('''CREATE TABLE tasks(
+							link text primary key,
+							title text,
+							tags text,
+							price text,
+							price_format text
+						)''')
+		conn.commit()
+	except sqlite3.OperationalError:
+		pass
+
+create_db()
 
 
-def add_user(id, keys, username, lang):
+def query_db(query, values=''):
 	conn = sqlite3.connect(db_loc)
 	with conn:
 		cursor = conn.cursor()
-		keys = ';'.join([''] + keys + ['']) 
-		conn.cursor().execute('INSERT INTO users(id, search_keys, nickname, lang, active) VALUES (?, ?, ?, ?, 1)', (id, keys, username, lang))
+		cursor.execute(query, values)
+		return cursor
+
+def add_user(user_id, keys, username, lang):
+	keys = ';'.join([''] + keys + ['']) 
+	query_db('INSERT INTO users(id, search_keys, nickname, lang, active) VALUES (?, ?, ?, ?, 1)', (user_id, keys, username, lang))
 
 def update_user_keys(id, keys):
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		conn.cursor().execute('UPDATE users SET search_keys=? WHERE id=?', (';'.join([''] + keys + ['']), id))
+	query_db('UPDATE users SET search_keys=? WHERE id=?', (';'.join([''] + keys + ['']), id))
 
 def get_users():
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		cursor.execute("SELECT id, search_keys, lang, nickname, active FROM users")
-		return [(User(row[0], row[1].split(";"), row[2], row[3], bool(row[4]))) for row in cursor.fetchall()]
+	cursor = query_db("SELECT id, search_keys, lang, nickname, active FROM users")
+	return [(User(row[0], row[1].split(";")[1:-1], row[2], row[3], bool(row[4]))) for row in cursor.fetchall()]
 
 def get_user_status(user_id):
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		cursor.execute("SELECT active FROM users WHERE id=?", (user_id,))
-		return cursor.fetchone()[0]
-
+	cursor = query_db("SELECT active FROM users WHERE id=?", (user_id,))
+	return cursor.fetchone()[0]
 def get_user_skeys(user_id):
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		cursor.execute("SELECT search_keys FROM users WHERE id=?", (user_id,))
-		try:
-			return cursor.fetchone()[0][1:-1].split(';')
-		except TypeError:
-			return []
-
-def get_relevant_users_ids(search_keys):
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		criteria = ' OR '.join([f"search_keys LIKE \"%;{key};%\"" for key in search_keys])
-		cursor.execute("SELECT id FROM users WHERE active=1 AND (" + criteria  + ")")
-		results = cursor.fetchall()
-		if results:
-			return [i[0] for i in results]
+	cursor = query_db("SELECT search_keys FROM users WHERE id=?", (user_id,))
+	try:
+		return cursor.fetchone()[0][1:-1].split(';')
+	except TypeError:
 		return []
 
+def get_relevant_users_ids(search_keys):
+	criteria = ' OR '.join([f"search_keys LIKE \"%;{key};%\"" for key in search_keys])
+	cursor = query_db("SELECT id FROM users WHERE active=1 AND (" + criteria  + ")")
+	results = cursor.fetchall()
+	if results:
+		return [i[0] for i in results]
+	return []
+
 def delete_user(user_id):
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		cursor.execute("DELETE FROM users WHERE id=?", (user_id,))
+	query_db("DELETE FROM users WHERE id=?", (user_id,))
 
 def add_task(task):
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		task['inline_tags'] = ';'.join(task['tags']) if task['tags'] else ''
-		cursor.execute("INSERT INTO tasks VALUES (:link, :title, :inline_tags, :price, :price_format)", task)
-		
+	task['inline_tags'] = ';'.join(task['tags']) if task['tags'] else ''
+	query_db("INSERT INTO tasks VALUES (:link, :title, :inline_tags, :price, :price_format)", task)
+
 def get_task(search_id):
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		cursor.execute('SELECT * FROM tasks WHERE link=?', (search_id,))
-		task = {}
-		task['link'], task['title'], task['tags'], task['price'], task['price_format'] = cursor.fetchone()
-		return task
+	cursor = query_db('SELECT * FROM tasks WHERE link=?', (search_id,))
+	task = {}
+	task['link'], task['title'], task['tags'], task['price'], task['price_format'] = cursor.fetchone()
+	return task
 
 def get_tasks_links(q=25):
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		cursor.execute('SELECT link FROM tasks ORDER BY link DESC')
-		return [i[0] for i in cursor.fetchmany(q)]
+	cursor = query_db('SELECT link FROM tasks ORDER BY link DESC')
+	return [i[0] for i in cursor.fetchmany(q)]
 
 def check_task_link(task_link):
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		cursor.execute('SELECT link FROM tasks WHERE link=?', (task_link,))
-		return bool(cursor.fetchone())
+	cursor = query_db('SELECT link FROM tasks WHERE link=?', (task_link,))
+	return bool(cursor.fetchone())
 
 def update_user_lang(user_id, lang):
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		cursor.execute('UPDATE users SET lang=? WHERE id=?', (lang, user_id))
+	query_db('UPDATE users SET lang=? WHERE id=?', (lang, user_id))
 
 def change_user_status(user_id, active=0):
-	conn = sqlite3.connect(db_loc)
-	with conn:
-		cursor = conn.cursor()
-		cursor.execute('UPDATE users SET active=? WHERE id=?', (active, user_id,)) 
+	query_db('UPDATE users SET active=? WHERE id=?', (active, user_id,))
